0afcdd95e6b8479703dd48828555a5c3e280fccc
Adercio <adercioreinanrocha@gmail.com>
2015-08-20 10:34:48 -0300

    Adicionado a policy de HA(Alta Disponibilidade)

--- provision-node2.sh
+++ provision-node2.sh
@@ -4,6 +4,8 @@ echo "192.168.0.100 node1" | sudo tee -a /etc/hosts
 
 sudo invoke-rc.d rabbitmq-server start
 
+sudo rabbitmqctl set_policy --priority 0 --apply-to queues HA-SYNC '^(?!amq.).*' '{"ha-mode": "all", "ha-sync-mode": "automatic"}'
+
 sudo rabbitmqctl stop_app
 
 sudo rabbitmqctl join_cluster rabbit@node1
